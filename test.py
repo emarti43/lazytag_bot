77f6a4e912727ac28ae0c933924582e591a6daee
Crystal Lin <crklin@ucsc.edu>
2019-02-09 13:45:54 -0800

    trying out fun stuff, add example to add by filename

--- test.py
+++ test.py
@@ -3,9 +3,11 @@ from clarifai.rest import ClarifaiApp
 
 app = ClarifaiApp(api_key='1f2c93de75074a088597cd7791491b5a')
 model = app.public_models.general_model
-response = model.predict_by_url('https://samples.clarifai.com/metro-north.jpg')
+response = model.predict_by_url('https://i.redd.it/640h9vc5gjf21.jpg')
+#response = model.predict_by_filename('~/yay')
 
 # print(response['outputs'][0]['data']['concepts'])
+
 
 name_map = []
 for dict_item in response['outputs'][0]['data']['concepts']:
