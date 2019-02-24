525772b74d77399f5fa06d768bc89cdf851b09ac
Camille Rogers <camillerogers215@gmail.com>
2019-02-23 16:14:37 -0800

    function to grab comments

--- grab.py
+++ grab.py
@@ -16,25 +16,28 @@ def get_comments_from_reddit_api(comment_ids,author):
     data = r.json()
     return data['data']['children']
 '''
-before = None
-
 ### IMPORTANT ######################
 # Set this variable to your username
-author = "stuck_in_the_matrix"
+# author = "stuck_in_the_matrix"
 ####################################
-comment_data = ""
-i = 0
-while i < 3:
-    comments = get_comments_from_pushshift(subreddit="foodporn", size=100,before=before,sort='desc',sort_type='created_utc')
-    if not comments: break
 
-    # This will get the comment ids from Pushshift in batches of 100 -- Reddit's API only allows 100 at a time
-    comment_bodies = ""
-    for comment in comments:
-        before = comment['created_utc'] # This will keep track of your position for the next call in the while loop
-        comment_bodies += comment['body'] + "\n"
+def get_comments(sr_name, comment_no):
+    i = 0
+    before = None
+    comment_data = ""
+    while i < 3:
+        comments = get_comments_from_pushshift(subreddit=sr_name, size=comment_no, before=before,sort='desc',sort_type='created_utc')
+        if not comments: break
 
-    # This will then pass the ids collected from Pushshift and query Reddit's API for the most up to date information
-    comment_data += comment_bodies
-    i+= 1
-    time.sleep(2)
+        # This will get the comment ids from Pushshift in batches of 100 -- Reddit's API only allows 100 at a time
+        comment_bodies = ""
+        for comment in comments:
+            before = comment['created_utc'] # This will keep track of your position for the next call in the while loop
+            comment_bodies += comment['body'] + "\n"
+        # This will then pass the ids collected from Pushshift and query Reddit's API for the most up to date information
+        comment_data += comment_bodies
+        i+= 1
+        time.sleep(2)
+    return comment_data
+
+print(get_comments("foodporn", 100))
